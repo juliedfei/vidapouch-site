6760e198fe0db2b1035fa74e92515f7bed3e6172
Julie Dawn <jdfeigs@gmail.com>
2026-07-26 19:15:13 -0400

    Add PostHog analytics

---
+++ app/instrumentation-client.ts
@@ -0,0 +1,26 @@
+import posthog from "posthog-js";
+
+const posthogToken =
+ process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN;
+
+const posthogHost =
+ process.env.NEXT_PUBLIC_POSTHOG_HOST;
+
+if (posthogToken) {
+ posthog.init(posthogToken, {
+   api_host:
+     posthogHost ??
+     "https://us.i.posthog.com",
+
+   capture_pageview: true,
+   capture_pageleave: true,
+
+   /*
+    * Protect customer-entered information
+    * in session recordings.
+    */
+   session_recording: {
+     maskAllInputs: true,
+   },
+ });
+}
